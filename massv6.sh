de48b8573f6b0e738fb6152802206126ab162085
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-02-09 17:19:41 +0200

    1

--- massv6.sh
+++ massv6.sh
@@ -9,12 +9,12 @@ else
   sudo apt update && sudo apt install curl -y < "/dev/null"
 fi
 #Ping ipv6
-ping6 ipv6.google.com > $HOME/ping6
+ping6 ipv6.google.com > $HOME/ping6 < "/dev/null"
 sleep 2
 
 #Set Var
 catt=/usr/bin/cat
-ping6=\$($catt \$HOME/ping6)
+ping6=$($catt \$HOME/ping6)
 if
     $ping6=""; then
     echo -e '\n\e[42mPlease enable ipv6\e[0m\n' && sleep 2
---
+++ readme.txt
@@ -0,0 +1,3 @@
+
+
+wget -q -O massv6.sh https://raw.githubusercontent.com/mgpwnz/VS/main/massv6.sh && chmod +x massv6.sh && ./massv6.sh
