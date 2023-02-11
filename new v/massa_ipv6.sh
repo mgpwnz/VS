a64d7bf751800ae910b9fb8a9544a63282cb4539
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-02-11 11:53:57 +0200

    add

---
+++ new v/massa_ipv6.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+while true
+do
+
+
+# Menu
+
+PS3='Select an action: '
+options=("Enable ipv6 Contabo" "Change massa config" "Exit")
+select opt in "${options[@]}"
+               do
+                   case $opt in                           
+
+"Enable ipv6 Contabo")
+echo "============================================================"
+echo "Enable ipv6 Contabo"
+echo "============================================================"
+# Change plan
+sed -i "/net.ipv6.conf.all.disable_ipv6.*/d" /etc/sysctl.conf && sysctl -q -p && echo 0 > /proc/sys/net/ipv6/conf/all/disable_ipv6 && sed -i "s/#//" /etc/netplan/01-netcfg.yaml && netplan generate && netplan apply
+break
+;;
+
+
+"Change massa config")
+
+echo "============================================================"
+echo "Enter your ipv6 address"
+echo "============================================================"
+read IPV6
+echo export IPV6=${IPV6} >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+#Make conf
+if [ ! -d $HOME/massa/massa-node/config/ ]; then
+    echo -e '\n\e[42m Massa is not installed!\e[0m\n'
+    exit
+    else
+rm $HOME/massa/massa-node/config/config.toml 
+  sleep 1
+
+tee <<EOF >/dev/null $HOME/massa/massa-node/config/config.toml
+routable_ip = "$IPV6"
+EOF
+
+sleep 1
+systemctl restart massad
+fi
+
+break
+;;
+
+"Exit")
+exit
+;;
+*) echo "invalid option $REPLY";;
+esac
+done
+done
