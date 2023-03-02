2383d0ac99c579dfa4d745a959fa605a072a9a07
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-03-02 13:09:13 +0200

    add

--- massa_ipv6.sh
+++ massa_ipv6.sh
@@ -7,12 +7,12 @@ do
 # Menu
 
 PS3='Select an action: '
-options=("Change massa config" "Enable ipv6 Contabo" "Exit")
+options=("Change massa config" "Enable ipv6 ONLY Contabo" "Enable autobuy rolls" "Detete autobuy rolls" "Exit")
 select opt in "${options[@]}"
                do
                    case $opt in                           
 
-"Enable ipv6 Contabo")
+"Enable ipv6 ONLY Contabo")
 # Change plan
 sed -i "/net.ipv6.conf.all.disable_ipv6.*/d" /etc/sysctl.conf && sysctl -q -p && echo 0 > /proc/sys/net/ipv6/conf/all/disable_ipv6 && sed -i "s/#//" /etc/netplan/01-netcfg.yaml && netplan generate && netplan apply
 echo " Ipv6 Enable"
@@ -35,7 +35,52 @@ else
 fi
 exit
 ;;
-
+"Enable autobuy rolls")
+# Create script 
+cd /root
+sleep 1
+sudo tee /root/rollsup.sh > /dev/null <<EOF
+#!/bin/sh
+#Версия 0.14
+cd /root/massa/massa-client
+#Set variables
+catt=/usr/bin/cat
+passwd=\$(\$catt \$HOME/massapasswd)
+candidat=\$(./massa-client wallet_info -p "\$passwd"|grep 'Rolls'|awk '{print \$4}'| sed 's/=/ /'|awk '{print \$2}')
+massa_wallet_address=\$(./massa-client -p "\$passwd" wallet_info |grep 'Address'|awk '{print \$2}')
+tmp_final_balans=\$(./massa-client -p "\$passwd" wallet_info |grep 'Balance'|awk '{print \$3}'| sed 's/=/ /'|sed 's/,/ /'|awk '{print \$2}')
+final_balans=\${tmp_final_balans%%.*}
+averagetmp=\$(\$catt /proc/loadavg | awk '{print \$1}')
+node=\$(./massa-client -p "\$passwd" get_status |grep 'Error'|awk '{print \$1}')
+if [ -z "\$node" ]&&[ -z "\$candidat" ];then
+echo \`/bin/date +"%b %d %H:%M"\` "(rollsup) Node is currently offline" >> /root/rolls.log
+elif [ \$candidat -gt "0" ];then
+echo "Ok" > /dev/null
+elif [ \$final_balans -gt "99" ]; then
+echo \`/bin/date +"%b %d %H:%M"\` "(rollsup) The roll flew off, we check the number of coins and try to buy" >> /root/rolls.log
+resp=\$(./massa-client -p "\$passwd" buy_rolls \$massa_wallet_address 1 0)
+else
+echo \`/bin/date +"%b %d %H:%M"\` "(rollsup) Not enough coins to buy a roll from you \$final_balans, minimum 100" >> /root/rolls.log
+fi
+EOF
+sleep 1
+printf "SHELL=/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+*/3 * * * * root /bin/bash /root/rollsup.sh > /dev/null 2>&1
+" > /etc/cron.d/massarolls
+sleep 1
+sudo tee $HOME/massapasswd > /dev/null <<EOF
+$massa_password
+EOF
+echo " script Enable"
+break
+;;
+"Delete autobuy rolls")
+/usr/bin/rm /root/rollsup.sh
+/usr/bin/rm /etc/cron.d/massarolls
+echo " Done"
+break
+;;
 "Exit")
 exit
 ;;
