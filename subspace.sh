bc1afeba1dd172be7987d150a9e333fe8af46262
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-03-14 20:41:16 +0200

    aaa

--- subspace.sh
+++ subspace.sh
@@ -79,17 +79,10 @@ sleep 1
   version: "3.7"
   services:
     node:
-      # For running on Aarch64 add `-aarch64` after `DATE`
       image: ghcr.io/subspace/node:gemini-3c-2023-mar-07
       volumes:
-  # Instead of specifying volume (which will store data in `/var/lib/docker`), you can
-  # alternatively specify path to the directory where files will be stored, just make
-  # sure everyone is allowed to write there
         - node-data:/var/subspace:rw
-  #      - /path/to/subspace-node:/var/subspace:rw
       ports:
-  # If port 30333 or 30433 is already occupied by another Substrate-based node, replace all
-  # occurrences of `30333` or `30433` in this file with another value
         - "0.0.0.0:32333:30333"
         - "0.0.0.0:32433:30433"
       restart: unless-stopped
@@ -107,12 +100,10 @@ sleep 1
         "--dsn-disable-private-ips",
         "--no-private-ipv4",
         "--validator",
-  # Replace `INSERT_YOUR_ID` with your node ID (will be shown in telemetry)
         "--name", "$SUBSPACE_NODE_NAME"
       ]
       healthcheck:
         timeout: 5s
-  # If node setup takes longer than expected, you want to increase `interval` and `retries` number.
         interval: 30s
         retries: 5
 
@@ -120,17 +111,10 @@ sleep 1
       depends_on:
         node:
           condition: service_healthy
-      # For running on Aarch64 add `-aarch64` after `DATE`
       image: ghcr.io/subspace/farmer:gemini-3c-2023-mar-07
       volumes:
-  # Instead of specifying volume (which will store data in `/var/lib/docker`), you can
-  # alternatively specify path to the directory where files will be stored, just make
-  # sure everyone is allowed to write there
         - farmer-data:/var/subspace:rw
-  #      - /path/to/subspace-farmer:/var/subspace:rw
       ports:
-  # If port 30533 is already occupied by something else, replace all
-  # occurrences of `30533` in this file with another value
         - "0.0.0.0:32533:30533"
       restart: unless-stopped
       command: [
@@ -139,9 +123,7 @@ sleep 1
         "--disable-private-ips",
         "--node-rpc-url", "ws://node:9944",
         "--listen-on", "/ip4/0.0.0.0/tcp/30533",
-  # Replace `WALLET_ADDRESS` with your Polkadot.js wallet address
         "--reward-address", "$SUBSPACE_WALLET_ADDRESS_ADDRESS",
-  # Replace `PLOT_SIZE` with plot size in gigabytes or terabytes, for instance 100G or 2T (but leave at least 60G of disk space for node and some for OS)
         "--plot-size", "$SUBSPACE_PLOT_SIZE"
       ]
   volumes:
@@ -260,6 +242,7 @@ uninstall() {
 cd $HOME/subspace && docker compose down -v
 sudo rm -rf $HOME/subspace 
 echo "Done"
+cd
 }
 # Actions
 sudo apt install tmux wget -y &>/dev/null
