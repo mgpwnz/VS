d642dc85f5dff60f974191f2b130d93b1d5b7b10
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-07-06 01:56:16 +0300

    cron

--- cron.sh
+++ cron.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 #create script
-
+wget -q -O status.sh https://raw.githubusercontent.com/mgpwnz/VS/main/status.sh && chmod +x status.sh && ./status.sh
 sleep 1
 #Add cron
 printf "SHELL=/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-*/15 * * * * root /bin/bash /root/status_shardeum.sh > /dev/null 2>&1
+*/1 * * * * root /bin/bash /root/status.sh > /dev/null 2>&1
 " > /etc/cron.d/status_shardeum
