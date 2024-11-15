e0f47230724bb2fa1a913fea775ed1df0031c8b6
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-11-15 14:39:05 +0200

    updt

--- telegram_monitor.sh
+++ telegram_monitor.sh
@@ -66,7 +66,7 @@ log_status() {
         fi
 
         # Get only the state line and extract the status
-        STATUS=\$(echo "\$STATUS_OUTPUT" | grep -oP 'state: \K(active|standby|stopped|ready|syncing|offline|waiting-for-network)' | tail -n 1)
+        STATUS=\$(echo "\$STATUS_OUTPUT" | grep -oP 'state: \K(active|standby|stopped|ready|syncing|offline|initializing|waiting-for-network)' | tail -n 1)
 
         # Get the current timestamp in UTC+2 (Kyiv)
         TIMESTAMP=\$(TZ=\$TIMEZONE date '+%Y-%m-%d %H:%M UTC+2')
@@ -177,7 +177,7 @@ check_status() {
         STATUS="unknown"
     else
         # Extract the node status
-        STATUS=\$(echo "\$STATUS_OUTPUT" | grep -oP 'state: \K(active|standby|stopped|ready|offline|syncing|waiting-for-network)' | tail -n 1)
+        STATUS=\$(echo "\$STATUS_OUTPUT" | grep -oP 'state: \K(active|standby|stopped|ready|offline|syncing|initializing|waiting-for-network)' | tail -n 1)
     fi
 
     HOSTNAME=\$(hostname)
@@ -200,6 +200,8 @@ check_status() {
         STATUS_EMOJI="🟠 ready"
     elif [ "\$STATUS" == "syncing" ]; then
         STATUS_EMOJI="♻️ syncing"
+    elif [ "\$STATUS" == "initializing" ]; then
+        STATUS_EMOJI="⚠️ Need stake SHM"
     else
         STATUS_EMOJI="unknown"
     fi
