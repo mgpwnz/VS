8f436cc8ae59929739d29e966ccba055a0057c0c
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2025-05-13 22:40:54 +0300

    upd

--- setup-aztec-validator.sh
+++ setup-aztec-validator.sh
@@ -18,7 +18,7 @@ read YourAddress
 echo -n "Enter run time (YYYY-MM-DD HH:MM:SS): "
 read SCHEDULE
 
-# --- Шаг 3: сохраняем переменные в файл в домашнем каталоге ---
+# --- Шаг 3: сохраняем переменные в файле в домашнем каталоге ---
 ENV_FILE="$HOME/.env_aztec-validator"
 cat > "$ENV_FILE" <<EOF
 RPC_URL=${RPC_URL}
@@ -45,12 +45,15 @@ Description=One-shot run of aztec add-l1-validator
 
 [Service]
 Type=oneshot
-EnvironmentFile=${ENV_FILE}
-ExecStart=${AZTEC_BIN} add-l1-validator \
-  --l1-rpc-urls "\${RPC_URL}" \
-  --private-key "\${YourPrivateKey}" \
-  --attester "\${YourAddress}" \
-  --proposer-eoa "\${YourAddress}" \
+User=root
+EnvironmentFile=$ENV_FILE
+Environment=HOME=/root
+WorkingDirectory=/root
+ExecStart=$AZTEC_BIN add-l1-validator \
+  --l1-rpc-urls "${RPC_URL}" \
+  --private-key "${YourPrivateKey}" \
+  --attester "${YourAddress}" \
+  --proposer-eoa "${YourAddress}" \
   --staking-asset-handler 0xF739D03e98e23A7B65940848aBA8921fF3bAc4b2 \
   --l1-chain-id 11155111
 EOF
@@ -58,10 +61,10 @@ EOF
 # --- Шаг 6: создаём или обновляем таймер с новым расписанием ---
 cat > "$TIMER_PATH" <<EOF
 [Unit]
-Description=Run aztec-validator.service at ${SCHEDULE}
+Description=Run aztec-validator.service at $SCHEDULE
 
 [Timer]
-OnCalendar=${SCHEDULE}
+OnCalendar=$SCHEDULE
 Persistent=true
 
 [Install]
